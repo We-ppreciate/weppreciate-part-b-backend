dfbf2d5541d718a0194329277c205116330935d9
Nate P <33781520+nate-0hz@users.noreply.github.com>
2023-12-21 15:20:00 +1100

    bugfix: validation error on releaseDate
    - fix validation on releaseDate for Nomination by removing superfluous validation from NominationModel
    - install @types/jest and @types/supertest
    - Remove commented code from NominationController
    - Remove commented code from UserController
    - Remove LogToFile from database.js
    - Remove LogToFile from index.js
    - Rework error message response on server.js
     - Purge incorrect copy of UserValidation

--- src/controllers/NominationController.js
+++ src/controllers/NominationController.js
@@ -8,40 +8,6 @@ const auth = require('../functions/verifyToken');
 const { validateNewNomination, validateUpdateNomination } = require('../validations/NominationValidation');
 const { errorSwitch } = require('./ErrorController');
 
-
-
-// /* REMOVE IF NOT BROKEN */
-
-
-// const errorSwitch = (err, response) => {
-//   let statusCode = 500;
-//   let message = `Sorry. That\'s a problem on our side. Look like Mavis spilled her tea on the server. ${err}`;
-
-//   switch (err) {
-//     case 400:
-//       statusCode = 400;
-//       message = `Your intent is good but the request was bad. ${err}`;
-//       break;
-//     case 403:
-//       statusCode = 403;
-//       message = `You are not authorised to do that. We'pologise. ${err}`;
-//       break;
-//     case 404:
-//       statusCode = 404;
-//       message = `This is not the page you are looking for. ${err}`;
-//       break;
-//     default:
-//       statusCode = 500;
-//       message = `Sorry. That's a problem on our side. Mavis is looking into it now... well, she will, after her tea. ${err}`
-//       break;      
-//   };
-
-//     response.status(statusCode).json({ message: message });
-//     logToFile(`UserController.js: ${err}`);
-//     console.log(`UserController.js: ${err}`);
-// };
-
-
   /* === NOMINATION GET ROUTES === */
 
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 // This file handles the start up of the server and the parameters 
-
-
 require('dotenv').config();
 
 const { databaseConnect } = require('./database');
-const { logToFile } = require('./functions/logToFile');
 const { app } = require('./server');
+const { errorSwitch } = require('./controllers/ErrorController');
 
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, async () => {
 	try {
 		await databaseConnect();
-		logToFile(`index.js: Server started on port ${PORT}`);
 		console.log(`index.js: Server started on port ${PORT}`);
 		console.log('We\'ppreciate you starting us up!');
 	} catch (err) {
-		logToFile(`index.js: ${err}`);
-		console.log(`index.js: ${err}`);
-
-		console.log(`We\'re sorry. There\'s a problem starting up. Mavis is looking into it now... well, she will, after her tea. ${err}`);
-	}
+    errorSwitch(err, response);
+  }
 });
 
 module.exports = { 
